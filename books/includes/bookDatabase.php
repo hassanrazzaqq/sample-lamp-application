81edd12157ea2241fd4a77a4928de3640ef3ece9
Hassan <53465335+hassanrazzaqq@users.noreply.github.com>
2021-09-11 21:23:09 +0500

    Update bookDatabase.php

--- books/includes/bookDatabase.php
+++ books/includes/bookDatabase.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "127.0.0.1";
-$username = "mysql";
-$password = "mysql";
+$username = "mysql_user";
+$password = "mysql_user";
 $dbname = "bookstore";
 
 $bookconn = mysqli_connect($servername, $username, $password, $dbname );
